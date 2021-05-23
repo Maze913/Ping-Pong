5307059b67ff050287b72eb1a4aee2ee7dd4b851
Maze913 <84269004+Maze913@users.noreply.github.com>
2021-05-23 14:32:01 +0300

    v2.0
    Adding Neon Effects
    Adding Music Effects

--- File.py
+++ File.py
@@ -1,12 +1,18 @@
 from pygame import *
 from random import *
+import time as tm
 #создай игру "Лабиринт"!
+
 
 #создай окно игры
 window = display.set_mode((700,500))
 display.set_caption("Ping-Pong")
 game = True
 finish = False
+score1 = 0
+score2 = 0
+mixer.init()
+kick = mixer.Sound('paddle.wav')
 #задай фон сцены
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, width, height):
@@ -32,47 +38,88 @@ class Player(GameSprite):
         if keys_pressed[K_DOWN] and self.rect.y < 390:
             self.rect.y += self.speed
 
-background = transform.scale(image.load("Green Path.jpg"), (700, 500))
-hero = Player('Player 1.png', 25, 400, 10, 25, 105)
+background = transform.scale(image.load("Field.png"), (700, 500))
+hero = Player('Player 1.png', 25, 300, 10, 25, 105)
 hero2 = Player('Player 2.png', 655, 400, 10, 25, 105)
-ball = GameSprite('Ball.png', 320, 100, 10, 25, 25)
+ball = GameSprite('Ball.png', 320, 200, 10, 25, 25)
 
 speed_x = 6
 speed_y = 6
-
-
 
 clock = time.Clock()
 FPS = 60
 font.init()
 font = font.SysFont('comic sans ms', 35)
-
+ddd = tm.time()
+def restart():
+    global score1, score2, finish    
+    finish = False
+    ball.rect.x = 320
+    ball.rect.y = 250
+    hero.rect.x = 25
+    hero.rect.y = 250
+    hero2.rect.x = 655
+    hero2.rect.y = 250
+    score1 = 0
+    score2 = 0
+    tekst = font.render(str(score1), True, (255, 0, 0))
+    text2 = font.render(str(score2), True, (0, 0, 255))
+    window.blit(text2, (600,50))
+    window.blit(tekst, (100,50))
+    
 while game:
+    if tm.time() - ddd > 5:
+        ddd = tm.time()
+        speed_x = speed_x + 1
     for e in event.get():
         if e.type == QUIT:
             game = False
+        keys_pressed = key.get_pressed()
     if not finish:
         window.blit(background,(0, 0))
+        tekst = font.render(str(score1), True, (255, 0, 0))
+        text2 = font.render(str(score2), True, (0, 0, 255))
+        window.blit(text2, (600,50))
+        window.blit(tekst, (100,50))
         ball.rect.x += speed_x
         ball.rect.y += speed_y
         if sprite.collide_rect(hero, ball) or sprite.collide_rect(hero2, ball):
             speed_x *= -1
             speed_y *= 1
+            kick.play()
         if ball.rect.y < 0 or ball.rect.y > 480:
             speed_y *= -1
         if ball.rect.x < 0:
-            finish = True
-            text = font.render("1 Игрок Победил!", True, (255, 0, 0))
-            window.blit(text, (255,225))
+            score2 = score2 + 1
+            ball.rect.x = 320
+            ball.rect.y = 100
         if ball.rect.x > 695:
+            score1 = score1 + 1
+            ball.rect.x = 320
+            ball.rect.y = 100
+        if score1 >= 5:
+            tekst = font.render(str(score1), True, (255, 0, 0))
+            text2 = font.render(str(score2), True, (0, 0, 255))
+            window.blit(text2, (600,50))
+            window.blit(tekst, (100,50))    
+            win1 = font.render('Красный Игрок Победил!', True, (255, 0, 0))
+            window.blit(win1, (255,200))
             finish = True
-            text = font.render("2 Игрок Победил!", True, (0, 0, 255))
-            window.blit(text, (255,225))
+        if score2 >= 5:
+            tekst = font.render(str(score1), True, (255, 0, 0))
+            text2 = font.render(str(score2), True, (0, 0, 255))
+            window.blit(text2, (600,50))
+            window.blit(tekst, (100,50))
+            win2 = font.render('Синий Игрок Победил!', True, (0, 0, 255))
+            window.blit(win2, (255,200))
+            finish = True
         hero.reset()
         hero.update()
         ball.reset()
         ball.update()
         hero2.reset()
         hero2.update2()
+    if keys_pressed[K_o]:
+        restart()
     display.update()
     clock.tick(FPS)
